b7cdbc53db35318c290c4e6746e9e7db31a466fe
DEVI S <devs@dhcp-10-120-153-88.wifi.oracle.com>
2024-07-12 12:43:07 +0530

    changed made for fail

--- src/main/java/com/training/bms/AlstormAppApplication.java
+++ src/main/java/com/training/bms/AlstormAppApplication.java
@@ -9,7 +9,7 @@ public class AlstormAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AlstormAppApplication.class, args);
 		System.out.println("#### Alstorm App Started - Friday ####");
-		System.out.println("Started automatically");
+		System.out.println("Started automatically after changes");
 		
 	}
 
